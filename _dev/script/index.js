de540d91054fa1957451e464ddb866ba271cba62
kensuke goto <kensuke.goto.1981@gmail.com>
2020-04-19 12:28:41 +0900

    追加：アメリカ地図を表示

--- _dev/script/index.js
+++ _dev/script/index.js
@@ -1,3 +1,31 @@
 import * as d3 from "d3";
 import * as topojson from "topojson-client";
 
+const svg = d3.select("svg");
+const width = +svg.attr("width"),
+      height = + svg.attr("height");
+
+const unemplyment = d3.map();
+const path = d3.geoPath();
+
+d3.json("./data/us-map.json").then(us => {
+  ready(us);
+})
+
+function ready(us){
+
+  svg.append("g")
+    .attr("class","countries")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter().append("path")
+    .attr("fill","red")
+    .attr("d",path)
+
+  svg.append("path")
+    .data([topojson.mesh(us,us.objects.states,(a,b) => {
+      return a !== b;
+    })])
+    .attr("class","states")
+    .attr("d",path)
+}
